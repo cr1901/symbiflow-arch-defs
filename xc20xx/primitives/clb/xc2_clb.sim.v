c07421aa9caed489b3fe95b0c30780b576c53523
William D. Jones <thor0505@comcast.net>
2018-07-14 23:37:39 -0400

    Add CLB template for XC20XX family.

---
+++ xc20xx/primitives/clb/xc2_clb.sim.v
@@ -0,0 +1,16 @@
+module XC2_CLB(
+    A, B, C, D, // INPUTS
+    X, Y, // OUTPUTS
+    K // CLK
+);
+    input wire A;
+    input wire B;
+    input wire C;
+    input wire D;
+
+    output wire X;
+    output wire Y;
+
+    input wire K;
+
+endmodule
